1cd69cfcecc26caeb707b2976b908e187ee09f0e
WJRTXBOY <A2779197552@outlook.com>
2025-12-09 15:43:10 +0800

    结构更新

---
+++ src/scripts/main.js
@@ -0,0 +1,17 @@
+// 1. 获取元素
+let button = document.getElementById("myButton");
+let text = document.getElementById("myText");
+
+// 2. 添加点击事件
+button.addEventListener("click", function() {
+    // 3. 修改元素内容
+    text.textContents = "文本被改变了！";
+    text.style.color = "red";
+  
+    // 4. 创建新元素
+    let newElement = document.createElement("div");
+    newElement.textContent = "这是新添加的内容";
+    console.log(newElement);
+    document.body.appendChild(newElement);
+});
+
